e26a4f7ae4e4d3bdc6e1ac99ab8f19779efc12b1
ixiaoma <33934020+ixiaoma@users.noreply.github.com>
2018-09-25 10:45:07 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -4,6 +4,11 @@
             <router-view v-if="$route.meta.keepAlive"></router-view>
           </keep-alive>
             <router-view v-if="!$route.meta.keepAlive"></router-view>
+            <--include{页面的class名称，v-if相当于两套路由在切换}
+            <keep-alive :include="cachePage">
+              <router-view></router-view>
+            </keep-alive>
+            -->
     </div>
 </template>
 
